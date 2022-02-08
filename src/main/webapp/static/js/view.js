fa56d5f6227a9358f5a28369ab387529cb02afa7
Tesszike99 <teszidani02@gmail.com>
2022-02-08 14:25:47 +0100

    Feature - cart

--- src/main/webapp/static/js/view.js
+++ src/main/webapp/static/js/view.js
@@ -1,12 +1,5 @@
-export {cartView};
+export {cartView}
 
-let productPrice = document.querySelectorAll(".product-price");
-function cartView(buttonId) {
-    for (let price of productPrice) {
-        let priceId = price.getAttribute("priceId");
-        if (priceId === buttonId) {
-            let priceInt = price.getAttribute("price");
-            console.log(priceInt);
-        }
-    }
+function cartView(price){
+    console.log(price + "LOOOOOOOOOOOl")
 }
