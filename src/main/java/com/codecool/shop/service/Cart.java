074b5d902b7006b6582f6710f22a22faa53f4ed3
kistegez <tegeza@gmail.com>
2022-02-09 02:26:24 +0100

    Fix - fixed List<Product> in cart class

--- src/main/java/com/codecool/shop/service/Cart.java
+++ src/main/java/com/codecool/shop/service/Cart.java
@@ -4,19 +4,17 @@ import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.model.BaseModel;
 import com.codecool.shop.model.Product;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Cart {
-    Map<String, Integer> products = new HashMap<String, Integer>();
+    List<Product> products = new ArrayList<>();
 
     public void add(int id){
         Product product = ProductDaoMem.getInstance().find(id);
-        String name = product.getName();
-            int quantity = products.getOrDefault(name, 0) + 1;
-            products.put(name, quantity);
-        System.out.println(products);
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
     }
 }
