9ed06b38bb525be03271b85fddc3852c8061a32e
kistegez <tegeza@gmail.com>
2022-02-10 15:15:40 +0100

    Style - PaymentPage CSS

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -1,6 +1,9 @@
 function setupEventListener() {
     document.getElementById("company_true").addEventListener("click",hideElement);
     document.getElementById("company_false").addEventListener("click",hideElement);
+    document.getElementById("paypal").addEventListener("click", hidePaying);
+    document.getElementById("CreditCardInput").addEventListener("click", hidePaying);
+
 }
 
 
@@ -10,6 +13,18 @@ function hideElement() {
         billingCompanyField.style.display = "block";
     } else {
         billingCompanyField.style.display = "none";
+    }
+}
+
+function hidePaying() {
+    let creditCardInput = document.getElementById("CreditCard");
+    let payPalInput = document.getElementById("payPal_input");
+    if (creditCardInput.style.display === "none") {
+        creditCardInput.style.display = "block";
+        payPalInput.style.display = "none"
+    } else {
+        creditCardInput.style.display = "none"
+        payPalInput.style.display = "block"
     }
 }
 
