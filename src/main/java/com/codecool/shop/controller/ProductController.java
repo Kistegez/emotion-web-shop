f13a5f474d642101d33b809f142240093225845c
kistegez <tegeza@gmail.com>
2022-02-24 14:12:19 +0100

    Feature - decide where to load it (jdbc or mem)

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,6 +1,13 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SetUpMemOrJdbc;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.database.ShopDatabaseManager;
+import com.codecool.shop.dao.memory.ProductCategoryDaoMem;
+import com.codecool.shop.dao.memory.ProductDaoMem;
+import com.codecool.shop.dao.memory.SupplierDaoMem;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
@@ -14,22 +21,35 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
+    ProductDao productDataStore;
+    ProductCategoryDao productCategoryDataStore;
+    SupplierDao supplierDataStore;
+    SetUpMemOrJdbc setUpMemOrJdbc = new SetUpMemOrJdbc();
+    boolean daoTypeIsJdbc;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //ProductDao productDataStore = ProductDaoMem.getInstance();
-        //ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        //ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
-        ShopDatabaseManager database = new ShopDatabaseManager();
-        try {
-            database.setup();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        this.daoTypeIsJdbc = setUpMemOrJdbc.readResource();
+        if(daoTypeIsJdbc){
+            ShopDatabaseManager database = new ShopDatabaseManager();
+            try {
+                database.setup();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            productDataStore = database.getProductDao();
+            productCategoryDataStore =  database.getProductCategoryDao();
+            supplierDataStore = database.getSupplierDao();
+        }else {
+            productDataStore = ProductDaoMem.getInstance();
+            productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+            supplierDataStore = SupplierDaoMem.getInstance();
         }
-        ProductService productService = new ProductService(database.getProductDao(), database.getProductCategoryDao(), database.getSupplierDao());
-
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDataStore);
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("products", productService.getAllProducts());
